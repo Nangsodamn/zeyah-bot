e68ac726ca2cb1b65e9a257bac9fdff02d52443d
Nangsodamn <nangmwa02@gmail.com>
2026-03-17 18:55:49 +0800

    Add autoReconnect configuration to zeyah.config.ts

--- zeyah.config.ts
+++ zeyah.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from "@zeyah-bot/registry";
 import "dotenv/config";
 import { sixSevenPlugin } from "./checked/sixSeven-plugin.js";
 import { menuHandlePlugin } from "./checked/menu-handle.js";
-autoReconnect: true,
+
 export default defineConfig({
   DESIGN: {
     Title: "Zeyah",
     Admin: "Nang AI",
     Theme: "retro",
   },
+
+  autoReconnect: true,
+  
   adminBot: ["1234346521881739307", "100075058221244"],
   moderatorBot: [],
   prefixes: [process.env.PREFIX ?? "+"],
